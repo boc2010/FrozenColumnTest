80f180565f71272afae119d230ec199627392059
Nguyễn Văn Bình <binhnv.net@outlook.com>
2023-10-07 10:13:47 +0700

    Add project files.

---
+++ FrozenColumn/Program.cs
@@ -0,0 +1,35 @@
+using FrozenColumn.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTA0NTk0NUAzMjMwMmUzNDJlMzBJZFc2UEt0cm5UV3NoVXlJT0ozME1YSlBRd01KcHB4a2RadGhNdVAwQU44PQ==");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
